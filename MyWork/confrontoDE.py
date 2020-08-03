4c79c7930c401f7a95644c506106350a67b10768
aur-ora <65078165+aur-ora@users.noreply.github.com>
2020-08-03 20:26:42 +0200

    Update confrontoDE.py

--- MyWork/confrontoDE.py
+++ MyWork/confrontoDE.py
@@ -1,11 +1,19 @@
 import stanza
 from spacy_stanza import StanzaLanguage
 
+#Uso di stanza per ricavare da una frase i lemmi, i pos, i ner e le dep-parse
+
 #stanza.download("de")
 snlp = stanza.Pipeline(lang="de")
 nlp = StanzaLanguage(snlp)
 
-doc = nlp("Der Junge isst in der Schule einen Apfel.")
+doc = nlp("Die Nelly, eine seetüchtige Jolle, schwoite an ihrem Anker ohne die leiseste Regung in den Segeln und hielt Rast.")
 for token in doc:
     print(token.text, token.lemma_, token.pos_, token.dep_, token.ent_type_)
 print(doc.ents)
+
+#Ricavare i lemmi (synset) delle parole --> i lemmi saranno in inglese anche se l'input è tedesco
+#così si possono confrontare
+from nltk.corpus import wordnet as wn
+lem = wn.lemmas("Anker", lang="deu")
+print(lem)
