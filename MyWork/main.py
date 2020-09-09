ec95bbf79d01846e88bd2af373295cc1c2482282
aur-ora <65078165+aur-ora@users.noreply.github.com>
2020-09-09 19:44:29 +0200

    Add files via upload

--- MyWork/main.py
+++ MyWork/main.py
@@ -1,16 +1,16 @@
 import MyWork
 # Defining main function
 def main():
-    enTXT = open("junger.marbledue.en.txt", "r")
-    deTXT = open("junger.marmordue.de.txt", "r")
+    enTXT = open("junger.marble.en.txt", "r")
+    deTXT = open("junger.marmor.de.txt", "r")
     fifth = open("sentences.txt", "r")
     funf = open("sätze.txt", "r")
     en = open("one.txt", "r")
     de = open("ein.txt", "r")
     #print((MyWork.createSet_en(englischTXT)))
     #print((MyWork.createSet_de(deutschTXT)))
-    print(MyWork.alignmentBabelNet.sameSentence(enTXT, deTXT))
-    #print(MyWork.getCoreference("conrad.darkness.en.txt"))
+    #print(MyWork.alignmentBabelNet.sameSentence(enTXT, deTXT))
+    print(MyWork.getCoreference("junger.marble.en.txt"))
 
 if __name__ == "__main__":
       main()
